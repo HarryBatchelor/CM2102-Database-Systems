453bda427394022782a1dfc939c405a03e2a8e73
HarryBatchelor <hmbatchelor8@gmail.com>
2019-12-02 22:59:00 +0000

    Almost

--- CW2/report.sql
+++ CW2/report.sql
@@ -1,6 +1,6 @@
 Clear screen;
 Accept Report_file char prompt ‘Enter a file name for summary report’;
-Col Storecode format A8 heading ‘Store Code’;
+Col StoreCode format A8 heading ‘Store Code’;
 Col Description format A8 heading ‘Item Description’;
 Col Price format $999999.99 heading ‘Price’;
 Col Quantity format 99999 heading ‘Quantity’;
@@ -14,14 +14,15 @@ Set linesize 100;
 Set wrap off;
 Set underline =;
 Ttitle center ‘Current Stock value by store’ skip 2 center –
-  ‘prepared by ‘ &your_name skip 2center -
+  ‘prepared by ‘ &your_name skip 2 center -
     _Date skip 2;
-Bittle center format 999 sql.pno;
+Btitle center format 999 sql.pno;
 Break on StoreCode skip 2 on Description skip 1 on report;
 Compute sum label 'Store Total' of (Price*Quantity) on StoreCode;
-Compute sum lable 'Grand Total' of (price*Quantity) on Report;
+Compute sum label 'Grand Total' of (price*Quantity) on Report;
 Select StoreCode, Description, Price, Quantity, (Price*Quantity)
 From stocks
+group by StoreCode, Description, Price, Quantity
 Order by Quantity desc;
 Clear break;
 Clear columns;
