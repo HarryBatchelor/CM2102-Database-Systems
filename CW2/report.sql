7bd3416e95b1f0bb1354fbfafec0a99fe60d6216
HarryBatchelor <hmbatchelor8@gmail.com>
2019-12-10 16:21:24 +0000

    almost done

--- CW2/report.sql
+++ CW2/report.sql
@@ -1,21 +1,19 @@
 Clear screen;
-Accept Report_file char prompt ‘Enter a file name for summary report’;
-Col StoreCode format A8 heading ‘Store Code’;
-Col Description format A8 heading ‘Item Description’;
-Col Price format $999999.99 heading ‘Price’;
-Col Quantity format 99999 heading ‘Quantity’;
-Col (Price*Quantity) format $999999.99 heading ‘Value’;
+Accept Report_file char prompt "Enter a file name for summary report";
+Col StoreCode format A8 heading "Store Code";
+Col Description format A8 heading "Item Description";
+Col Price format $999999.99 heading "Price";
+Col Quantity format 99999 heading "Quantity";
+Col (Price*Quantity) format $999999.99 heading "Value";
 Set pause off;
 Set feedback off;
 Set space 6;
-Set newpage 2;
-Set pagesize 30;
+Set newpage 4;
+Set pagesize 100;
 Set linesize 100;
 Set wrap off;
 Set underline =;
-Ttitle center ‘Current Stock value by store’ skip 2 center –
-  ‘prepared by ‘ &your_name skip 2 center -
-    _Date skip 2;
+Ttitle center "Current Stock value by store" skip 2 center - center _Date skip 2 - center "Prepared by " &your_name;
 Btitle center format 999 sql.pno;
 Break on StoreCode skip 2 on Description skip 1 on report;
 Compute sum label 'Store Total' of (Price*Quantity) on StoreCode;
@@ -23,6 +21,8 @@ Compute sum label 'Grand Total' of (price*Quantity) on Report;
 Select StoreCode, Description, Price, Quantity, (Price*Quantity)
 From stocks
 order by StoreCode, SuppCode;
+spool off;
 Clear break;
 Clear columns;
 Clear computes;
+set Pause on;
